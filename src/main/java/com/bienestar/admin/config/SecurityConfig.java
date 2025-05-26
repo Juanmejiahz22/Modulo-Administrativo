1ce8823a39d105cdc39d7f9b04051f55c469f7e8
Juan Jose <juan.mejia-c@mail.escuelaing.edu.co>
2025-05-26 00:16:23 -0500

    Corrige security

--- src/main/java/com/bienestar/admin/config/SecurityConfig.java
+++ src/main/java/com/bienestar/admin/config/SecurityConfig.java
@@ -1,36 +1,32 @@
 package com.bienestar.admin.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.bienestar.admin.filter.JwtFilter;
 
 @Configuration
 @EnableWebSecurity 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private JwtFilter jwtFilter;
+    //@Autowired
+    //private JwtFilter jwtFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-          .csrf().disable()                      // deshabilita CSRF
+          .csrf().disable()                     
           .authorizeRequests()
-            .antMatchers("/api/usuarios/login")  // libera solo este endpoint
-              .permitAll()
-            .anyRequest()                        // el resto requiere token
-              .authenticated()
+            //.antMatchers("/api/usuarios/login")  // libera solo este endpoint
+              .anyRequest().permitAll()
+                                  
+              //.authenticated()
           .and()
-          .formLogin().disable()                 // desactiva el login HTML
-          .httpBasic().disable();                // desactiva Basic Auth
+          .formLogin().disable()              
+          .httpBasic().disable();                
 
         // antes de que llegue UsernamePasswordAuthenticationFilter
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        //http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
 
